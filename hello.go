9afc1e58a1e4364477e98f4cd0ddf7880994db29
Dushyant Rijhwani <dushyantrijhwani@gmail.com>
2016-03-07 16:01:37 +0100

    Initial commit

---
+++ hello.go
@@ -0,0 +1,97 @@
+package hello
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+)
+
+type Greeting struct {
+	Author  string
+	Content string
+	Date    time.Time
+}
+
+type GuestBookVars struct {
+	Greetings []Greeting
+	User      user.User
+}
+
+var (
+	guestbookTemplate = template.Must(template.ParseFiles("guestbook.html"))
+)
+
+func init() {
+	http.HandleFunc("/sign", sign)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/", root)
+}
+
+// guestbookKey returns the key used for all guestbook entries.
+func guestbookKey(c appengine.Context) *datastore.Key {
+	// The string "default_guestbook" here could be varied to have multiple guestbooks.
+	return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
+	u := user.Current(c)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	vars := GuestBookVars{
+		Greetings: greetings,
+	}
+
+	if u != nil {
+		vars.User = *u
+	}
+	if err := guestbookTemplate.Execute(w, vars); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func sign(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	g := Greeting{
+		Content: r.FormValue("content"),
+		Date:    time.Now(),
+	}
+
+	if u := user.Current(c); u != nil {
+		g.Author = u.String()
+	}
+
+	key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+	_, err := datastore.Put(c, key, &g)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello, %v!", u)
+}
